380bd017e2f4a66778aeaa27d75a4e3a9dd4ff01
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-12-28 21:08:51 +0530

    Added: Save data in database file is added

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -2,26 +2,43 @@ package com.bridgelabz.employeepayrollapp.model;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import lombok.Data;
-
-@Data
-public class EmployeePayrollData {
-
+@Entity
+@Table(name="employee_payroll")
+public @Data class EmployeePayrollData {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "employee_id")
 	private int employeeId;
+	
+	@Column(name= "name")
 	private String name;
 	private String gender;
 	private long salary;
 	private LocalDate startdate;
 	private String note;
 	private String profilePic;
+	
+	@ElementCollection
+	@CollectionTable(name ="employee_department", joinColumns =  @JoinColumn(name = "id"))
+	@Column(name = "department")
 	private List<String>departments;
 	
 	
 	public EmployeePayrollData() {}
 
-	public EmployeePayrollData(int empId, EmployeePayrollDTO empPayRollDTO) {
-		this.employeeId = empId;
+	public EmployeePayrollData(EmployeePayrollDTO empPayRollDTO) {
 		this.updateEmployeePayrollData(empPayRollDTO);
 	}
 
